ad9d4bd171b9d8fb8ae2aa3207599cb4753b776b
Lovro <hodak.lovro@gmail.com>
2020-12-08 13:24:49 +0100

    myBack first push

---
+++ data.js
@@ -0,0 +1,36 @@
+let students = [
+    {
+        name: 'Lovro',
+        age: 10
+    },
+    {
+        name: 'Jan',
+        age: 30
+    },
+    {
+        name: 'Nejc',
+        age: 22
+    }
+]
+
+let teachers = [
+    {
+        name: 'Veronika',
+        age: 15
+    },
+    {
+        name: 'Kim',
+        age: 35
+    },
+    {
+        name: 'Tjasa',
+        age: 25
+    }
+]
+
+const printName
+
+
+//module.exports = students
+module.exports = { students, teachers}
+
---
+++ index.js
@@ -0,0 +1,6 @@
+let data = require('./data')
+console.log(data) // node index.js
+
+let { students, teachers } = require('./data')
+console.log(students, teachers) // node index.js
+
